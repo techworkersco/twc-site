30d3401537b41e327e4e104c0c124b96c1ba1b8c
Yonatan Miller <yonatan@shushugah.com>
2020-12-24 04:00:02 +0100

    Add aria attributes to buttons, and deepen color contrast to make Lighthouse happy Remove redundant jquery function

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,19 +2,10 @@ $(document).ready(function() {
   $('#navBtn').on('click', function(event) {
     $('#nav').toggleClass('hide');
     $('#langNav').addClass('hide');
-    $('#chapterNav').addClass('hide');
   });
 
   $('#langBtn').on('click', function(event) {
     $('#langNav').toggleClass('hide');
     $('#nav').addClass('hide');
-    $('#chapterNav').addClass('hide');
-  });
-
-  $('#chapterBtn').on('click', function(event) {
-    $('#chapterNav').toggleClass('hide');
-    $('#nav').addClass('hide');
-    $('#langNav').addClass('hide');
   });
 })
-
