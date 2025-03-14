9541eae55a96dba464dd708efa80999617a00d6f
yonatan miller <yonatan@shushugah.com>
2025-03-14 21:04:47 +0100

    Times up (#413)
    Now renders correct day of the month (relative to time zone) for events
    without an image<img width="674" alt="Screenshot 2025-03-14 at 19 42 30"
    src="https://github.com/user-attachments/assets/c95d7e79-2c35-4bc9-8a6f-db24e225a79e"
    />
    
    And more impactful, if an event is in US Time zones, us am/pm formatting
    instead of 24/hour time zone.
    <img width="767" alt="Screenshot 2025-03-14 at 19 42 50"
    src="https://github.com/user-attachments/assets/d997870f-70fb-498b-8f71-a651e181330d"
    />
    
    ---------
    
    Co-authored-by: rmlibre <rmlibre@riseup.net>

--- _plugins/time_zones.rb
+++ _plugins/time_zones.rb
@@ -2,27 +2,36 @@ require 'tzinfo'
 
 module Jekyll
   module TimeZones
+    AM_PM_TIME_ZONES = ["US/Eastern", "US/Pacific", "US/Central", "US/Mountain"]
+
     def time_converter_url(date)
       "https://www.timeanddate.com/worldclock/converter.html?iso=" +
       date.utc.strftime("%Y%m%dT%H%M%S") +
       "&p1=179&p2=224&p3=37"   
     end
 
-    def first_time_zone(date, timezones)
-      timezones ||= %w[Europe/Berlin]  
-      date.getlocal(
-        TZInfo::Timezone.get(timezones.first)
-      ).strftime("%R %Z")   
+    def format_time(time, time_zone)
+      if AM_PM_TIME_ZONES.include?(time_zone) 
+        time.strftime("%-I:%M%P %Z")
+      else
+        time.strftime("%R %Z")
+      end
+    end  
+
+    def relative_day_of_month(date, timezones)
+      date.getlocal(TZInfo::Timezone.get(timezones.first)).strftime("%d")
     end
 
     def all_time_zones(date, timezones)
       date = date.to_time
       timezones ||= %w[Europe/Berlin]
 
-      timezones.map { |tz|   
-        date.getlocal(
-          TZInfo::Timezone.get(tz)
-        ).strftime("%R %Z")
+      timezones.map { |tz|      
+        format_time(
+          date.getlocal(
+            TZInfo::Timezone.get(tz)
+          ), tz
+        )
       }.join(", ").prepend dmy(date, timezones)
     end
 
---
+++ merch.md
@@ -0,0 +1,11 @@
+---
+layout: page
+title: Merchandise and swag
+permalink: /merch/
+---
+
+# {{page.title}}
+
+If you want to show your support to Tech Workers Coalition, you can purchase and wear TWC merchandise. We are selling t-shirts (available in USA only for now) via [Worx Printing](https://shop.worxprinting.coop/products/tech-workers-coalition-shirt), a worker-owned and union-printing shop.
+
+![Black t-shirt with TWC logo in white](/assets/img/twc-tshirt.jpg)
