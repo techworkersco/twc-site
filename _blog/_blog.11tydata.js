ac804ad72b51504a848b7691ced52c6ec2420510
maximsmol <1472826+maximsmol@users.noreply.github.com>
2026-01-14 15:37:20 -0800

    properly fix blog dates
    Signed-off-by: maximsmol <1472826+maximsmol@users.noreply.github.com>

--- _blog/_blog.11tydata.js
+++ _blog/_blog.11tydata.js
@@ -6,27 +6,6 @@ const dateRe = /^(\d{4})-(\d{2})-(\d{2})-(.*)$/;
 export default {
   tags: ["blog"],
   layout: "newsletter",
-  eleventyComputed: {
-    date(data) {
-      const {
-        page: { inputPath, date },
-      } = data;
-      if (date != null) return date;
-
-      const parsed = path.parse(inputPath);
-      const m = dateRe.exec(parsed.name);
-      if (m == null)
-        throw new Error(
-          `Could not parse blog date from filename: ${parsed.name}`,
-        );
-
-      const res = DateTime.fromISO(`${m[1]}-${m[2]}-${m[3]}T00:00:00`, {
-        zone: "America/New_York",
-      }).toJSDate();
-      data.page.date = res;
-      return res;
-    },
-  },
   permalink(data) {
     const {
       page: { inputPath },
