f69ba9c2e728aec5f63f4351c51d00e98231b74d
Stefan <shodgeskluck@gmail.com>
2026-03-11 15:08:13 -0400

    adding link to new contact form

--- _layouts/default.11ty.tsx
+++ _layouts/default.11ty.tsx
@@ -248,7 +248,7 @@ export const render = async ({
                   </li>
                 ))}
                 <li>
-                  <a href="mailto:hello@techworkerscoalition.org">Email Us</a>
+                  <a href="/contact-us">Contact Us</a>
                 </li>
               </ul>
             </div>
